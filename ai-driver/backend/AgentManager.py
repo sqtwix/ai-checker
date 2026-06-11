6fe2b3b2df067d04278db558d6f755fb1e63c687
SpunchBob <aleksandrprokudin2007@gmail.com>
2026-06-11 12:29:18 +0300

    feat(Agent): Write simple AgentManager for input data processing

--- ai-driver/backend/AgentManager.py
+++ ai-driver/backend/AgentManager.py
@@ -1,18 +1,56 @@
 from AgentClient import AgentClient
 from AgentFactory import AgentFactory
+import json
+import concurrent.futures
 
 class AgentManager:
     def __init__(agent_factory : AgentFactory, self):
-        self.agent_factory = agent_factory
-    
-    def setup_deepseek_queue(self):
-        self.deepseek_queue : list[AgentClient] = self.agent_factory.create_queue("deepseek")
+        try:
+            self.agent_factory = agent_factory
+            self.deepseek_queue = self.agent_factory.create_queue("deepseek")
+            self.sbergpt_queue = self.agent_factory.create_queue("sbergpt")
+            with open("system_prompts.json", "r") as f:
+                row_context = f.read()
+                self.system_prompts = json.loads(row_context)
+        except Exception as e:
+            raise Exception("Agent Managet Initialization Error: " + e.__str__())
 
-    def setup_sbergpt_queue(self):
-        self.sbergpt_queue : list[AgentClient] = self.agent_factory.create_queue("sbergpt")
+    def start_deepseek_processing(input_data : str, self) -> str:
+        try:
+            ai_responses = {
+                "main-analyzer" : "",
+                "anomalies-analyzer" : "",
+                "statistics-summarizer" : ""
+            }
 
-    def start_deepseek_processing(input_data : str, self):
-        
+            agents_count = len(self.deepseek_queue)
+
+            for specialization in ai_responses.keys():
+                agent_idx = 0
+                with concurrent.futures.ThreadPoolExecutor() as executor:
+                    future = executor.submit(self.deepseek_queue[agent_idx].execute, (self.system_prompts[agent_idx]['prompt'], input_data))
+                    ai_responses[specialization] = future.result()
+
+            return json.dumps(ai_responses)
+        except Exception as e:
+            raise Exception("DeepSeek Processing Error: " + e.__str__())
 
     def start_sbergpt_processing(input_data : str, self):
-        ...
+        try:
+            ai_responses = {
+                "main-analyzer" : "",
+                "anomalies-analyzer" : "",
+                "statistics-summarizer" : ""
+            }
+
+            agents_count = len(self.sbergpt_queue)
+
+            for specialization in ai_responses.keys():
+                agent_idx = 0
+                with concurrent.futures.ThreadPoolExecutor() as executor:
+                    future = executor.submit(self.sbergpt_queue[agent_idx].execute, (self.system_prompts[agent_idx]['prompt'], input_data))
+                    ai_responses[specialization] = future.result()
+
+            return json.dumps(ai_responses)
+        except Exception as e:
+            raise Exception("SberGpt Processing Error: " + e.__str__())
