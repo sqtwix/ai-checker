e93212c88ffd216e5f373f300bc19da5a3893a36
SpunchBob <aleksandrprokudin2007@gmail.com>
2026-06-11 11:43:09 +0300

    feat(Agent): created file json with system prompts

--- ai-driver/backend/AgentManager.py
+++ ai-driver/backend/AgentManager.py
@@ -2,8 +2,8 @@ from AgentClient import AgentClient
 from AgentFactory import AgentFactory
 
 class AgentManager:
-    def __init__(self):
-        self.agent_factory = AgentFactory()
+    def __init__(agent_factory : AgentFactory, self):
+        self.agent_factory = agent_factory
     
     def setup_deepseek_queue(self):
         self.deepseek_queue : list[AgentClient] = self.agent_factory.create_queue("deepseek")
@@ -12,7 +12,7 @@ class AgentManager:
         self.sbergpt_queue : list[AgentClient] = self.agent_factory.create_queue("sbergpt")
 
     def start_deepseek_processing(input_data : str, self):
-        ...
+        
 
     def start_sbergpt_processing(input_data : str, self):
         ...
